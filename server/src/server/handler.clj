f0670303671da20c9767fa839c3aa0b6ff690712
Henning Jacobs <henning@jacobs1.de>
2015-01-30 15:14:27 +0100

    code format

--- server/src/server/handler.clj
+++ server/src/server/handler.clj
@@ -14,25 +14,31 @@
                                      :bind-dn (:ldap-bind-dn env)
                                      :password (:ldap-password env)
                                      :ssl? (Boolean/parseBoolean (:ldap-ssl env))
-                                    }))
+                                     }))
+
+(defn get-ldap-user-dn [name]
+  "Build LDAP DN for a given user name"
+  (str "uid=" name "," (:ldap-base-dn env)))
 
 (defn get-public-key [name]
+  "Get a user's public SSH key"
   (let [conn (ldap-connect)]
-            (:sshPublicKey (ldap/get conn (str "uid=" name "," (:ldap-base-dn env))) [:sshPublicKey])
-  ))
+    (:sshPublicKey (ldap/get conn (get-ldap-user-dn name) [:sshPublicKey]))))
 
 
 (defn serve-public-key [name]
   (if (re-matches user-name-pattern name)
-      (get-public-key name)
-      {:status 400
-       :body "Invalid user name"}))
+    (or (get-public-key name)
+        {:status 404
+         :body "User not found"})
+    {:status 400
+     :body "Invalid user name"}))
 
 (defroutes app-routes
-  (GET "/" [] "SSH Access Granting Service")
-  (GET "/health" [] "OK")
-  (GET "/public-keys/:name/sshkey.pub" [name] (serve-public-key name))
-  (route/not-found "Not Found"))
+           (GET "/" [] "SSH Access Granting Service")
+           (GET "/health" [] "OK")
+           (GET "/public-keys/:name/sshkey.pub" [name] (serve-public-key name))
+           (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
