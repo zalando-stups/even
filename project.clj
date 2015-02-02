fcbd30036dbfdceae154373dcda53c1367d7578a
Henning Jacobs <henning@jacobs1.de>
2015-02-02 10:18:24 +0100

    Travic CI config

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject server "0.1.0-SNAPSHOT"
   :description "SSH public key server"
   :url "https://github.com/zalando/ssh-access-granting-service"
+  :license {:name "Apache License"
+            :url "http://www.apache.org/licenses/"}
+  :scm {:url "git@github.com:zalando/ssh-access-granting-service.git"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  ; lifecycle management
@@ -12,6 +15,11 @@
                  [metosin/compojure-api "0.16.4"]
                  [metosin/ring-http-response "0.5.2"]
                  [metosin/ring-swagger-ui "2.0.17"]
+                 ; logging
+                 [org.clojure/tools.logging "0.2.4"]
+                 [org.slf4j/slf4j-api "1.7.7"]
+                 [org.slf4j/jul-to-slf4j "1.7.7"]
+                 [org.slf4j/jcl-over-slf4j "1.7.7"]
                  ; LDAP
                  [org.clojars.pntblnk/clj-ldap "0.0.9"]
 
@@ -23,9 +31,16 @@
   :aliases {"cloverage" ["with-profile" "test" "cloverage"]}
 
   :ring {:handler server.handler/app}
-  :aot :all
   :main server.handler
+  :aot :all
   :uberjar-name "ssh-access-granting-service.jar"
-  :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+  :profiles {
+             :log {:dependencies [[org.apache.logging.log4j/log4j-core "2.1"]
+                                  [org.apache.logging.log4j/log4j-slf4j-impl "2.1"]]}
+             :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]}
+             :test [:log {:dependencies [[org.clojars.runa/conjure "2.1.3"]
+                                         [midje "1.6.3"]
+                                         [clj-http "1.0.1"]
+                                         [ring-mock "0.1.5"]]}]
+             :repl [:no-log]})
