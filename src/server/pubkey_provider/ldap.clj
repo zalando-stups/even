47b66580169f133717a261c593da9e2b6de634ac
Henning Jacobs <henning@jacobs1.de>
2015-02-09 16:32:53 +0100

    ldap-connect is needed

--- src/server/pubkey_provider/ldap.clj
+++ src/server/pubkey_provider/ldap.clj
@@ -1,42 +1,43 @@
 (ns server.pubkey-provider.ldap
-    (:require
-      [clojure.tools.logging :as log]
-      [com.stuartsierra.component :as component]
-      [clj-ldap.client :as ldap]
-      [server.config :as config])
-    )
+  (:require
+    [clojure.tools.logging :as log]
+    [com.stuartsierra.component :as component]
+    [clj-ldap.client :as ldap]
+    [server.config :as config])
+  )
 
 (defrecord Ldap [config pool])
 
 (defn get-ldap-user-dn [name {:keys [base-dn]}]
-      "Build LDAP DN for a given user name"
-      (str "uid=" name "," base-dn))
+  "Build LDAP DN for a given user name"
+  (str "uid=" name "," base-dn))
 
 (defn ldap-config [{:keys [host bind-dn password ssl connect-timeout]}]
-      {:host host
-       :bind-dn bind-dn
-       :password password
-       :ssl? (Boolean/parseBoolean ssl)
-       :connect-timeout (Integer/parseInt (or connect-timeout "10000"))})
+  {:host host
+   :bind-dn bind-dn
+   :password password
+   :ssl? (Boolean/parseBoolean ssl)
+   :connect-timeout (Integer/parseInt (or connect-timeout "10000"))})
 
 (defn ldap-connect [{:keys [config pool] :as ldap-server}]
-      (if pool
-        pool
-        (let [ldap-config (ldap-config config)]
-             (log/info "Connecting to LDAP server " (config/mask ldap-config) + " ..")
-             (let [conn (ldap/connect ldap-config)]
-                  (assoc ldap-server :pool conn)
-                  conn)
-             )))
+  (if pool
+    pool
+    (let [ldap-config (ldap-config config)]
+      (log/info "Connecting to LDAP server " (config/mask ldap-config) + " ..")
+      (let [conn (ldap/connect ldap-config)]
+        (assoc ldap-server :pool conn)
+        conn)
+      )))
 
-(defn ldap-auth? [{:keys [username password]} {:keys [config pool]}]
-  (ldap/bind? pool (get-ldap-user-dn username config) password))
+(defn ldap-auth? [{:keys [username password]} {:keys [config] :as ldap-server}]
+  (let [conn (ldap-connect ldap-server)]
+    (ldap/bind? conn (get-ldap-user-dn username config) password)))
 
 (defn get-public-key [name {:keys [config] :as ldap-server}]
-      "Get a user's public SSH key"
-      (let [conn (ldap-connect ldap-server)]
-           (:sshPublicKey (ldap/get conn (get-ldap-user-dn name config) [:sshPublicKey]))))
+  "Get a user's public SSH key"
+  (let [conn (ldap-connect ldap-server)]
+    (:sshPublicKey (ldap/get conn (get-ldap-user-dn name config) [:sshPublicKey]))))
 
 (defn ^Ldap new-ldap [config]
-      (log/info "Configuring LDAP with" (config/mask config))
-      (map->Ldap {:config config}))
+  (log/info "Configuring LDAP with" (config/mask config))
+  (map->Ldap {:config config}))
