6753b9884fbd19aa11fc5fdd084b793a694ce196
Andre Hartmann <Andre.Hartmann@zalando.de>
2015-06-08 16:31:37 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -7,18 +7,18 @@
   :min-lein-version "2.0.0"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.zalando.stups/friboo "0.20.0"]
-                 [metosin/ring-http-response "0.5.2"]
+                 [org.zalando.stups/friboo "0.21.0"]
+                 [metosin/ring-http-response "0.6.2"]
                  ; LDAP
                  [org.clojars.pntblnk/clj-ldap "0.0.9"]
                  ; SSH client
                  [clj-ssh "0.5.11"]
-                 [amazonica "0.3.19"]
+                 [amazonica "0.3.23"]
 
-                 [yesql "0.5.0-rc2"]
+                 [yesql "0.5.0-rc3"]
                  [squirrel "0.1.1"]
 
-                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.codec "0.1.0"]
                  [com.brweber2/clj-dns "0.0.2"]
                  [commons-net/commons-net "3.3"]]
@@ -51,12 +51,12 @@
   :profiles {:uberjar {:aot :all}
 
              :test    {:dependencies [[clj-http-lite "0.2.1"]
-                                      [org.clojure/java.jdbc "0.3.6"]]}
+                                      [org.clojure/java.jdbc "0.3.7"]]}
 
              :dev     {:repl-options {:init-ns user}
                        :source-paths ["dev"]
                        :dependencies [[org.clojure/tools.namespace "0.2.10"]
                                       [org.clojure/java.classpath "0.2.2"]
                                       [clj-http-lite "0.2.1"]
-                                      [org.clojure/java.jdbc "0.3.6"]]}})
+                                      [org.clojure/java.jdbc "0.3.7"]]}})
 
